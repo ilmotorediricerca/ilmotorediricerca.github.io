328541fe039fa3e1341e2775b356dcdaf06b63a4
roorco <roorco@gmail.com>
2021-05-16 19:11:15 +0200

    first commit

---
+++ assets/js/hover.js
@@ -0,0 +1,7 @@
+$(document).ready( function(){
+    $(".gold").hover(function(){
+      $('.gold').css("background-color", "var(--oc-yellow-5)");
+      }, function(){
+      $('.gold').css("background-color", "white");
+    })
+});
